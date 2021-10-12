7af72ceecce8182be685e340053f3ea5c89011cd
Plamen Andreev <plam4u@me.com>
2021-10-12 14:43:56 +0200

    Initial 'precondition' config adoption

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/config.h
@@ -19,8 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL
 #define MASTER_LEFT
-//#define EE_HANDS
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+// Left-hand home row mods
+#define HOME_A LCTL_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RCTL_T(KC_SCLN)
+
+#define CTL OSM(MOD_LCTL)
+#define SHIFT OSM(MOD_LSFT)
+#define ALT OSM(MOD_LALT)
+#define CMD OSM(MOD_LGUI)
+
+#define CTL_Z CTL_T(KC_Z)
+#define CTL_SL RCTL_T(KC_SLSH)
+#define CM_LBRC SGUI(KC_LBRC)
+#define CM_RBRC SGUI(KC_RBRC)
+
