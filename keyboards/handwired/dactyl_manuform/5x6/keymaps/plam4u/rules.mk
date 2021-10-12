2a5bcf277217ad6e771370a28d3d2d08d39613f8
Plamen Andreev <plam4u@me.com>
2021-10-12 14:58:49 +0200

    Provide keymapviz config for readibility

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/rules.mk
@@ -1,5 +1,5 @@
 # Automatically update the keymap visualization when compiling
 .PHONY: %
 %:
-	keymapviz -k dactyl_manuform5x6 -t fancy -r keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c > /dev/null
+	keymapviz -k dactyl_manuform5x6 -t fancy -r keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c -c keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymapviz.toml > /dev/null
 
