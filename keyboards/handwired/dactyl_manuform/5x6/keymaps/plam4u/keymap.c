42d8ed4190021ccc53f639da59bd3996358e91e5
Plamen Andreev <plam4u@me.com>
2021-11-16 03:00:55 +0100

    Replace defined with dynamic combo length

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
@@ -106,14 +106,17 @@ enum combos {
   JK_TAB,
   QW_SFT,
   DF_LAYER,
+  COMBO_LENGTH
 };
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
 
-combo_t key_combos[COMBO_COUNT] = {
+combo_t key_combos[] = {
   [AB_ESC] = COMBO(ab_combo, KC_ESC),
   [JK_TAB] = COMBO(jk_combo, KC_TAB),
   [QW_SFT] = COMBO(qw_combo, KC_LSFT),
