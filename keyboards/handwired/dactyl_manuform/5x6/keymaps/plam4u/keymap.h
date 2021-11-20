b5bdea0adac5876e3e84d78540f02d2ad14cf8b3
Plamen Andreev <plam4u@me.com>
2021-11-20 10:01:09 +0100

    Improve layout

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.h
@@ -2,20 +2,13 @@
 
 #include QMK_KEYBOARD_H
 
-// Initialize variable holding the binary
-// representation of active modifiers.
 uint8_t mod_state;
 uint8_t oneshot_mod_state;
-uint16_t last_keycode;
-
-bool caps_word_on;
-void caps_word_enable(void);
-void caps_word_disable(void);
 
 enum layer_names {
     _QWERTY,
-    _NUMPAD,
     _SYM_NAV,
+    _NUMPAD,
     _ADJUST,
 };
 
