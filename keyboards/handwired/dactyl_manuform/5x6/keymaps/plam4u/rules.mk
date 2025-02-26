d36db80929ff7def28af27b1f6050efcd6727924
Plamen Andreev <plam4u@me.com>
2025-02-27 00:15:17 +0100

    Remove keymapviz usage

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/rules.mk
@@ -1,7 +1,7 @@
 # Automatically update the keymap visualization when compiling
-.PHONY: %
-%:
-	keymapviz -k dactyl_manuform5x6 -t fancy -r keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c -c keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymapviz.toml > /dev/null
+# .PHONY: %
+# %:
+# 	keymapviz -k dactyl_manuform5x6 -t fancy -r keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c -c keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymapviz.toml > /dev/null
 
 COMBO_ENABLE = no
 COMMAND_ENABLE = no
