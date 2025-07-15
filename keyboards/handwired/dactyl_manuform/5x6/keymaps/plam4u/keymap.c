1747deeb2907915a265bcf4e7f8dd15a69ee737a
Plamen Andreev <plam4u@me.com>
2025-07-15 10:36:57 +0200

    Remove duplicate rule entry

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
@@ -481,8 +481,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
 
-
-
         case ESC_LW1:
         case TD_LWR1:
             if (!record->event.pressed) {
