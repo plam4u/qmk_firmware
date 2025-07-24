c7df40957aaa74f626c8dd3c535a43c83ca7e2ff
Plamen Andreev <plam4u@me.com>
2025-07-24 15:54:14 +0200

    Add numbers to gaming layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/plam4u/keymap.c
@@ -202,11 +202,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_DF_GAMING] = LAYOUT_5x6(
-        _______, _______, _______, _______, _______, _______,        TO_QWER, _______, _______, _______, _______, _______,
+        _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,        TO_QWER, _______, _______, _______, _______, _______,
 
-        KC_ESC , KC_T   , KC_Q   , KC_W   , KC_E   , KC_R   ,        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_COLN,
-        KC_TAB , KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,        KC_H   , KC_J   , KC_K   , KC_L   , TD_CLN , KC_QUOT,
-        KC_ENT , KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   ,        KC_N   , KC_M   , KC_COMM, KC_DOT , CTL_SL , ALT_BSL,
+        KC_ENT , KC_T   , KC_Q   , KC_W   , KC_E   , KC_R   ,        KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_COLN,
+        KC_ESC , KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,        KC_H   , KC_J   , KC_K   , KC_L   , TD_CLN , KC_QUOT,
+        KC_LALT, KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   ,        KC_N   , KC_M   , KC_COMM, KC_DOT , CTL_SL , ALT_BSL,
                           KC_BSPC, KC_TAB ,                                            KC_EQL , KC_MINS,
                                             KC_ESC , KC_SPC ,        LT_AM_E, LT_SA_B,
                                             KC_LCTL, KC_LSFT,        OS_RSFT, OS_RCTL,
