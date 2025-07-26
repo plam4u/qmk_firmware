5bba62e0eca853927c45eafc6ce3f67f8bc80f1d
Plamen Andreev <plam4u@me.com>
2025-07-26 08:27:10 +0200

    Move adam's _FUN layer further down

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/adam/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/adam/keymap.c
@@ -94,13 +94,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  MO_FUN,LT_NAV_SPACE,MO_NUM,                    OS_SHFT,MO_NAV, MO_SYMB
   ),
 
-  [_FUN] = LAYOUT_split_3x5_3(
-       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            TO(_NAVLH), KC_F7, KC_F8, KC_F9, KC_F12,
-     OS_GUI,  OS_ALT, OS_CTRL, OS_SHFT, _______,               _______, KC_F4, KC_F5, KC_F6, KC_F11,
-       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,               _______, KC_F1, KC_F2, KC_F3, KC_F10,
-                      _______, _______, _______,               _______, KC_CAPS, _______
-  ),
-
   [_NAV] = LAYOUT_split_3x5_3(
      SW_WIN, PRV_TAB, CLS_WIN, NXT_TAB,     SW_APP,            KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DWRD,
      OS_GUI, OS_ALT,  OS_CTRL, OS_SHFT, OSL_NAVWIN,            KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_SPC,
@@ -113,6 +106,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     OS_GUI , OS_ALT , OS_CTRL,   OS_SHFT,     MO_NAV,             REV_COLON, KC_4, KC_5, KC_6,   KC_MINS,
     MW_UNDO, _______, KC_COMM, REV_COLON,    MW_REDO,               KC_MINS, KC_1, KC_2, KC_3,   _______,
                       _______,   _______,    _______,                KC_ENT, KC_BSPC, KC_0
+  ),
+
+  [_FUN] = LAYOUT_split_3x5_3(
+       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            TO(_NAVLH), KC_F7, KC_F8, KC_F9, KC_F12,
+     OS_GUI,  OS_ALT, OS_CTRL, OS_SHFT, _______,               _______, KC_F4, KC_F5, KC_F6, KC_F11,
+       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,               _______, KC_F1, KC_F2, KC_F3, KC_F10,
+                      _______, _______, _______,               _______, KC_CAPS, _______
   ),
 
   [_SYMB] = LAYOUT_split_3x5_3(
